1c741499dde5fcb5de651cf5cc26485e58eefbb4
xiaokaixin <1040580896@qq.com>
2022-08-07 16:03:57 +0800

    提交

---
+++ codes/spring-security-14cors/src/main/java/com/blr/filters/WebMvcConfig.java
@@ -0,0 +1,29 @@
+// package com.blr.filters;
+//
+//
+// import org.springframework.boot.web.servlet.FilterRegistrationBean;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+// import org.springframework.web.filter.CorsFilter;
+//
+// import java.util.Collections;
+//
+// @Configuration
+// public class WebMvcConfig {
+//     @Bean
+//     FilterRegistrationBean<CorsFilter> corsFilter() {
+//         FilterRegistrationBean<CorsFilter> registrationBean = new FilterRegistrationBean<>();
+//         CorsConfiguration corsConfiguration = new CorsConfiguration();
+//         corsConfiguration.setAllowedHeaders(Collections.singletonList("*"));
+//         corsConfiguration.setAllowedMethods(Collections.singletonList("*"));
+//         corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
+//         corsConfiguration.setMaxAge(3600L);
+//         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//         source.registerCorsConfiguration("/**", corsConfiguration);
+//         registrationBean.setFilter(new CorsFilter(source));
+//         registrationBean.setOrder(-1);//filter 0 1
+//         return registrationBean;
+//     }
+// }
